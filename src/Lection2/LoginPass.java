6667f86d7e5984a798a1cbd22473ae9eb3b4ec3b
portner-boop <damirmagdeev1054@gmail.com>
2024-10-16 18:31:07 +0400

    Lecture_1

---
+++ src/Lection2/LoginPass.java
@@ -0,0 +1,10 @@
+package Lection2;
+
+public class LoginPass extends Exception {
+    public LoginPass(){
+        super();
+    }
+    public LoginPass(String message){
+        super(message);
+    }
+}
